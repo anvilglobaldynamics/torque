78b392e9e9a3ec53401bc7c820a74dd45937494e
Sayem Shafayet <ishafayet@gmail.com>
2017-10-16 17:28:59 +0600

    Added template manager

---
+++ server/src/template-manager.js
@@ -0,0 +1,42 @@
+
+let fslib = require('fs');
+let Handlebars = require('handlebars');
+let { AsyncCollector } = require('baselib');
+let pathlib = require('path')
+
+class TemplateManager {
+
+  constructor() {
+  }
+
+  _loadAndPrepareTemplates(cbfn) {
+    let templateList = [
+      {
+        name: 'generic-message',
+        path: './src/templates/public/generic-message.html'
+      }
+    ]
+    let collector = new AsyncCollector(templateList.length);
+    for (let template of templateList) {
+      let html = fslib.readFileSync(template.path, { encoding: 'utf8' });
+      let compiledTemplate = Handlebars.compile(html);
+      collector.collect(template.name, { compiledTemplate: compiledTemplate });
+    }
+    collector.finally((collection) => {
+      this.templates = collection;
+      cbfn();
+    })
+  }
+
+  generateHtml(templateName, model) {
+    return this.templates[templateName].compiledTemplate(model);
+  }
+
+  initialize(cbfn) {
+    this._loadAndPrepareTemplates(_ => {
+      cbfn();
+    })
+  }
+}
+
+exports.TemplateManager = TemplateManager;
