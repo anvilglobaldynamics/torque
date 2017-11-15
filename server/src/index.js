13661dc41b5bf54fb9037a07bab42701f105cab7
Sayem Shafayet <ishafayet@gmail.com>
2017-11-15 16:22:02 +0600

    Added State-control feature to Server (Wrapped into Program Class)

--- server/src/index.js
+++ server/src/index.js
@@ -18,71 +18,99 @@ let config, logger, database, server, emailService, templateManager;
 
 let mode = detectMode();
 
-Promise.resolve()
-  .then(() => {
-    return promisify(ConfigLoader, ConfigLoader.getComputedConfig);
-  })
-  .then(([nonFatalErrorList, _config]) => {
-    ConfigLoader.reportErrorAndConfig(nonFatalErrorList, _config, mode);
-    config = _config;
-    return Promise.resolve();
-  })
-  .then(() => {
-    server = new Server(config, mode);
-    database = new Database(config.db.path);
-    logger = new Logger(config.log);
-    emailService = new EmailService(config.email);
-    templateManager = new TemplateManager();
-    return promisify(logger, logger.initialize);
-  })
-  .then(() => {
-    logger.info(`(server)> ${config.baseName} Started in ${mode} mode.`);
-    logger.info('(server)> logger initialized.');
-    server.setLogger(logger);
-    return Promise.resolve();
-  })
-  .then(() => {
-    return promisify(database, database.initialize);
-  })
-  .then(() => {
-    logger.info('(server)> database initialized.');
-    server.setDatabase(database);
-    return Promise.resolve();
-  })
-  .then(() => {
-    logger.info('(server)> initializing server.')
-    return promisify(server, server.initialize);
-  })
-  .then(() => {
-    logger.info('(server)> server initialized.')
-    return Promise.resolve();
-  })
-  .then(() => {
-    logger.info('(server)> registering APIs');
-    server.registerGetApi('/verify-email/:link', VerifyEmailApi);
-    server.registerPostApi('/api/user-register', UserRegisterApi);
-    server.registerPostApi('/api/user-login', UserLoginApi);
-    server.registerPostApi('/api/user-logout', UserLogoutApi);
-    return Promise.resolve();
-  })
-  .then(() => {
-    return promisify(templateManager, templateManager.initialize);
-  })
-  .then(() => {
-    logger.info('(server)> template manager initialized.')
-    server.setTemplateManager(templateManager);
-    return Promise.resolve();
-  })
-  .then(() => {
-    return promisify(emailService, emailService.initialize, logger);
-  })
-  .then(() => {
-    logger.info('(server)> email services initialized.')
-    server.setEmailService(emailService);
-    return Promise.resolve();
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+class Program {
+
+  constructor({ allowUnsafeApis = false, muteLogger = false }) {
+    this.allowUnsafeApis = allowUnsafeApis;
+    this.muteLogger =  muteLogger;
+  }
+
+  // NOTE: Intended to be used during testing
+  deleteDocByIdFromDb(collection, id) {
+
+  }
+
+  // NOTE: Intended to be used during testing or by process manager
+  terminateServer() {
+    console.log("Server Terminated Programmatically.");
+    process.exit(0);
+  }
+
+  initiateServer(callback) {
+    Promise.resolve()
+      .then(() => {
+        return promisify(ConfigLoader, ConfigLoader.getComputedConfig);
+      })
+      .then(([nonFatalErrorList, _config]) => {
+        if (!this.muteLogger){
+          ConfigLoader.reportErrorAndConfig(nonFatalErrorList, _config, mode);
+        }
+        config = _config;
+        return Promise.resolve();
+      })
+      .then(() => {
+        server = new Server(config, mode);
+        database = new Database(config.db.path);
+        logger = new Logger(config.log, this.muteLogger);
+        emailService = new EmailService(config.email);
+        templateManager = new TemplateManager();
+        return promisify(logger, logger.initialize);
+      })
+      .then(() => {
+        logger.info(`(server)> ${config.baseName} Started in ${mode} mode.`);
+        logger.info('(server)> logger initialized.');
+        server.setLogger(logger);
+        return Promise.resolve();
+      })
+      .then(() => {
+        return promisify(database, database.initialize);
+      })
+      .then(() => {
+        logger.info('(server)> database initialized.');
+        server.setDatabase(database);
+        return Promise.resolve();
+      })
+      .then(() => {
+        logger.info('(server)> initializing server.')
+        return promisify(server, server.initialize);
+      })
+      .then(() => {
+        logger.info('(server)> server initialized.')
+        return Promise.resolve();
+      })
+      .then(() => {
+        logger.info('(server)> registering APIs');
+        server.registerGetApi('/verify-email/:link', VerifyEmailApi);
+        server.registerPostApi('/api/user-register', UserRegisterApi);
+        server.registerPostApi('/api/user-login', UserLoginApi);
+        server.registerPostApi('/api/user-logout', UserLogoutApi);
+        return Promise.resolve();
+      })
+      .then(() => {
+        return promisify(templateManager, templateManager.initialize);
+      })
+      .then(() => {
+        logger.info('(server)> template manager initialized.')
+        server.setTemplateManager(templateManager);
+        return Promise.resolve();
+      })
+      .then(() => {
+        return promisify(emailService, emailService.initialize, logger);
+      })
+      .then(() => {
+        logger.info('(server)> email services initialized.')
+        server.setEmailService(emailService);
+        callback();
+        return Promise.resolve();
+      })
+      .catch((err) => {
+        console.error(err);
+        process.exit(1);
+      });
+  }
+
+}
+
+exports.Program = Program;
+
 
