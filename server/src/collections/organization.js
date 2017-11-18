3e6d3c87784f0a9eb01c0d339aeda71dbb6eb009
Sayem Shafayet <ishafayet@gmail.com>
2017-11-18 10:36:36 +0600

    Updated organization collection

--- server/src/collections/organization.js
+++ server/src/collections/organization.js
@@ -1,12 +1,16 @@
 
+const { Collection } = require('./../collection-base');
 const Joi = require('joi');
 
-let { ensureKeysAreUnique } = require('./../utils/ensure-unique');
 
-exports.organizationMixin = (DatabaseClass) => class extends DatabaseClass {
+exports.OrganizationCollection = class extends Collection {
 
-  get organizationSchema() {
-    return Joi.object().keys({
+  constructor(...args) {
+    super(...args);
+
+    this.collectionName = 'organization';
+
+    this.joiSchema = Joi.object().keys({
       createdDatetimeStamp: Joi.number().max(999999999999999).required(),
       lastModifiedDatetimeStamp: Joi.number().max(999999999999999).required(),
       name: Joi.string().min(1).max(64).required(),
@@ -16,55 +20,29 @@ exports.organizationMixin = (DatabaseClass) => class extends DatabaseClass {
       licenceExpiresOnDatetimeStamp: Joi.number().max(999999999999999).required(),
       isDeleted: Joi.boolean().required()
     });
+
+    this.uniqueDefList = [
+      {
+        additionalQueryFilters: {},
+        uniqueKeyList: ['email', 'phone']
+      }
+    ]
   }
 
-  _validateOrganization(doc, cbfn) {
-    let { error: err } = Joi.validate(doc, this.organizationSchema, {
-      convert: false
-    });
-    if (err) return cbfn(err);
-    let uniqueKeyList = ['email', 'phone']
-    ensureKeysAreUnique(this, 'organization', {}, doc, uniqueKeyList, (err) => {
-      if (err) return cbfn(err);
-      cbfn();
-    });
-  }
-
-  _insertOrganization(doc, cbfn) {
-    this._validateOrganization(doc, (err) => {
-      if (err) return cbfn(err);
-      this.autoGenerateKey('organization', (err, organizationId) => {
-        if (err) return cbfn(err);
-        doc.id = organizationId;
-        this.insertOne('organization', doc, (err, count) => {
-          if (err) return cbfn(err);
-          if (count !== 1) return cbfn(new Error("Could not insert organization for reasons unknown."));
-          return cbfn(null, organizationId);
-        });
-      });
-    });
-  }
-
-  _updateOrganization(query, modifications, cbfn) {
-    this.update('organization', query, modifications, cbfn);
-  }
-
-  // public:
-
-  createOrganization({ name, primaryBusinessAddress, phone, email, licenceExpiresOnDatetimeStamp }, cbfn) {
-    let organization = {
+  create({ name, primaryBusinessAddress, phone, email, licenceExpiresOnDatetimeStamp }, cbfn) {
+    let user = {
       createdDatetimeStamp: (new Date).getTime(),
       lastModifiedDatetimeStamp: (new Date).getTime(),
-      name, 
-      primaryBusinessAddress, 
-      phone, 
+      name,
+      primaryBusinessAddress,
+      phone,
       email,
       licenceExpiresOnDatetimeStamp,
       isDeleted: false
     }
-    this._insertOrganization(organization, (err, id) => {
+    this._insert(user, (err, id) => {
       return cbfn(err, id);
     })
   }
-
+  
 }
