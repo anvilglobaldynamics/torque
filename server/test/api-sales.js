60ae2f1411feaa42a0d4a98b44704f28e0c951ff
Mujtaba Nowshad <mujtabanowshad@gmail.com>
2017-12-10 20:45:37 +0600

    wip

--- server/test/api-sales.js
+++ server/test/api-sales.js
@@ -143,178 +143,35 @@ describe('sales', _ => {
 
   // });
 
-  // it('api/get-aggregated-inventory-details (Valid warehouse)', testDoneFn => {
+  it('api/get-aggregated-inventory-details (Valid sales check)', testDoneFn => {
 
-  //   callApi('api/get-aggregated-inventory-details', {
-  //     json: {
-  //       apiKey,
-  //       inventoryId: warehouseDefaultInventoryId
-  //     }
-  //   }, (err, response, body) => {
-  //     expect(response.statusCode).to.equal(200);
-  //     expect(body).to.have.property('hasError').that.equals(false);
-  //     expect(body).to.have.property('productList').that.is.an('array');
-  //     expect(body).to.have.property('matchingProductList').that.is.an('array');
-  //     expect(body).to.have.property('matchingProductCategoryList').that.is.an('array');
+    callApi('api/get-aggregated-inventory-details', {
+      json: {
+        apiKey,
+        inventoryId: outletDefaultInventoryId
+      }
+    }, (err, response, body) => {
+      console.log(body);
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.have.property('hasError').that.equals(false);
+      expect(body).to.have.property('productList').that.is.an('array');
+      expect(body).to.have.property('matchingProductList').that.is.an('array');
+      expect(body).to.have.property('matchingProductCategoryList').that.is.an('array');
 
-  //     productToBeTransferredId = body.productList[0].productId;
+      body.matchingProductList.forEach(product => {
+        validateProductSchema(product);
+      });
+      body.matchingProductCategoryList.forEach(productCategory => {
+        validateProductCategorySchema(productCategory);
+      });
 
-  //     body.matchingProductList.forEach(product => {
-  //       validateProductSchema(product);
-  //     });
-  //     body.matchingProductCategoryList.forEach(productCategory => {
-  //       validateProductCategorySchema(productCategory);
-  //     });
+      expect(body.matchingProductList[0]).to.have.property('productCategoryId').that.equals(productCategoryId);
+      expect(body.productList[0]).to.have.property('count').that.equals(100);
 
-  //     testDoneFn();
-  //   });
+      testDoneFn();
+    });
 
-  // });
-
-  // it('api/transfer-between-inventories (Valid)', testDoneFn => {
-
-  //   callApi('api/transfer-between-inventories', {
-  //     json: {
-  //       apiKey,
-  //       fromInventoryId: warehouseDefaultInventoryId,
-  //       toInventoryId: warehouseReturnedInventoryId,
-  //       productList: [
-  //         { productId: productToBeTransferredId, count: 1 }
-  //       ]
-  //     }
-  //   }, (err, response, body) => {
-  //     expect(response.statusCode).to.equal(200);
-  //     expect(body).to.have.property('hasError').that.equals(false);
-  //     testDoneFn();
-  //   });
-
-  // });
-
-  // it('api/transfer-between-inventories (Valid duplicate)', testDoneFn => {
-
-  //   callApi('api/transfer-between-inventories', {
-  //     json: {
-  //       apiKey,
-  //       fromInventoryId: warehouseDefaultInventoryId,
-  //       toInventoryId: warehouseReturnedInventoryId,
-  //       productList: [
-  //         { productId: productToBeTransferredId, count: 1 }
-  //       ]
-  //     }
-  //   }, (err, response, body) => {
-  //     expect(response.statusCode).to.equal(200);
-  //     expect(body).to.have.property('hasError').that.equals(false);
-  //     testDoneFn();
-  //   });
-
-  // });
-
-  // it('api/get-aggregated-inventory-details (Valid modification check)', testDoneFn => {
-
-  //   callApi('api/get-aggregated-inventory-details', {
-  //     json: {
-  //       apiKey,
-  //       inventoryId: warehouseReturnedInventoryId
-  //     }
-  //   }, (err, response, body) => {
-  //     expect(response.statusCode).to.equal(200);
-  //     expect(body).to.have.property('hasError').that.equals(false);
-  //     expect(body).to.have.property('productList').that.is.an('array');
-  //     expect(body).to.have.property('matchingProductList').that.is.an('array');
-  //     expect(body).to.have.property('matchingProductCategoryList').that.is.an('array');
-
-  //     body.matchingProductList.forEach(product => {
-  //       validateProductSchema(product);
-  //     });
-  //     body.matchingProductCategoryList.forEach(productCategory => {
-  //       validateProductCategorySchema(productCategory);
-  //     });
-
-  //     expect(body.productList[0]).to.have.property('productId').that.equals(productToBeTransferredId);
-  //     expect(body.productList[0]).to.have.property('count').that.equals(2);
-
-  //     testDoneFn();
-  //   });
-
-  // });
-
-
-  // it('api/transfer-between-inventories (Valid Warehouse to Outlet)', testDoneFn => {
-
-  //   callApi('api/transfer-between-inventories', {
-  //     json: {
-  //       apiKey,
-  //       fromInventoryId: warehouseDefaultInventoryId,
-  //       toInventoryId: outletDefaultInventoryId,
-  //       productList: [
-  //         { productId: productToBeTransferredId, count: 3 }
-  //       ]
-  //     }
-  //   }, (err, response, body) => {
-  //     expect(response.statusCode).to.equal(200);
-  //     expect(body).to.have.property('hasError').that.equals(false);
-  //     testDoneFn();
-  //   });
-
-  // });
-
-  // it('api/get-aggregated-inventory-details (Valid modification check, Outlet)', testDoneFn => {
-
-  //   callApi('api/get-aggregated-inventory-details', {
-  //     json: {
-  //       apiKey,
-  //       inventoryId: outletDefaultInventoryId
-  //     }
-  //   }, (err, response, body) => {
-  //     expect(response.statusCode).to.equal(200);
-  //     expect(body).to.have.property('hasError').that.equals(false);
-  //     expect(body).to.have.property('productList').that.is.an('array');
-  //     expect(body).to.have.property('matchingProductList').that.is.an('array');
-  //     expect(body).to.have.property('matchingProductCategoryList').that.is.an('array');
-
-  //     body.matchingProductList.forEach(product => {
-  //       validateProductSchema(product);
-  //     });
-  //     body.matchingProductCategoryList.forEach(productCategory => {
-  //       validateProductCategorySchema(productCategory);
-  //     });
-
-  //     expect(body.productList[0]).to.have.property('productId').that.equals(productToBeTransferredId);
-  //     expect(body.productList[0]).to.have.property('count').that.equals(3);
-
-  //     testDoneFn();
-  //   });
-
-  // });
-
-  // it('api/get-aggregated-inventory-details (Valid modification check, Warehouse)', testDoneFn => {
-
-  //   callApi('api/get-aggregated-inventory-details', {
-  //     json: {
-  //       apiKey,
-  //       inventoryId: warehouseDefaultInventoryId
-  //     }
-  //   }, (err, response, body) => {
-  //     expect(response.statusCode).to.equal(200);
-  //     expect(body).to.have.property('hasError').that.equals(false);
-  //     expect(body).to.have.property('productList').that.is.an('array');
-  //     expect(body).to.have.property('matchingProductList').that.is.an('array');
-  //     expect(body).to.have.property('matchingProductCategoryList').that.is.an('array');
-
-  //     body.matchingProductList.forEach(product => {
-  //       validateProductSchema(product);
-  //     });
-  //     body.matchingProductCategoryList.forEach(productCategory => {
-  //       validateProductCategorySchema(productCategory);
-  //     });
-
-  //     expect(body.productList[0]).to.have.property('productId').that.equals(productToBeTransferredId);
-  //     expect(body.productList[0]).to.have.property('count').that.equals(5);
-
-  //     testDoneFn();
-  //   });
-
-  // });
+  });
 
   it('END', testDoneFn => {
     terminateServer(testDoneFn);
