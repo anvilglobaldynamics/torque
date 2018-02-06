94331ba1a3c65c6408efe185d86c4fcd2100a2b8
Sayem Shafayet <ishafayet@gmail.com>
2018-02-06 11:37:27 +0600

    Added productCommonMixin#_verifyProductsExist

---
+++ server/src/apis/mixins/product-common.js
@@ -0,0 +1,20 @@
+
+exports.productCommonMixin = (SuperApiClass) => class extends SuperApiClass {
+
+  _verifyProductsExist(productList, cbfn) {
+    let productIdList = productList.map(product => product.productId);
+    this.database.product.findByIdList({ idList: productIdList }, (err, _productList) => {
+      if (err) return this.fail(err);
+      if (productList.length !== _productList.length) {
+        err = new Error("Unable to find all products in productList");
+        err.code = "PORDUCT_INVALID";
+        return this.fail(err);
+      }
+      return cbfn();
+    });
+
+  }
+
+
+
+}
