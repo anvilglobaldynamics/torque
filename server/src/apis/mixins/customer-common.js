57459d46442227017e754d04c2b98d674f93524a
Mujtaba Nowshad <mujtabanowshad@gmail.com>
2018-02-19 12:33:52 +0600

    bug fixed

--- server/src/apis/mixins/customer-common.js
+++ server/src/apis/mixins/customer-common.js
@@ -2,11 +2,15 @@
 exports.customerCommonMixin = (SuperApiClass) => class extends SuperApiClass {
 
   _getCustomer({ customerId }, cbfn) {
-    this.database.customer.findById({ customerId }, (err, customer) => {
-      if (err) return this.fail(err);
-      if (!this._ensureDoc(err, customer, "CUSTOMER_INVALID", "Customer not found.")) return;
-      return cbfn(customer);
-    });
+    if (customerId) {
+      this.database.customer.findById({ customerId }, (err, customer) => {
+        if (err) return this.fail(err);
+        if (!this._ensureDoc(err, customer, "CUSTOMER_INVALID", "Customer not found.")) return;
+        return cbfn(customer);
+      });
+    } else {
+      return cbfn(null);
+    }
   }
 
   _updateCustomerBalance({ diff, customer }, cbfn) {
