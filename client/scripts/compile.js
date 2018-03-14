d482e76546796919b8fffc233815336bb6c6feb8
Sayem Shafayet <ishafayet@gmail.com>
2018-03-14 14:37:50 +0600

    Attempting to configure Service Worker (Failing partially)

--- client/scripts/compile.js
+++ client/scripts/compile.js
@@ -2,19 +2,30 @@
 const shell = require('shelljs');
 const { hashLinks } = require('./hashlinks');
 const { updateBuildNumber } = require('./build-number');
+const pathlib = require('path');
 
 const runPolymerBuild = () => {
   let res = shell.exec('polymer build')
   if (res.code !== 0) {
-    shell.echo('Error: Git commit failed');
+    shell.echo('Error: polymer build failed');
+    shell.exit(1);
+  }
+}
+
+const publishPwa = ()=>{
+  let res = shell.exec('gcloud app deploy pwa.yaml --quiet');
+  if (res.code !== 0) {
+    shell.echo('Error: gcloud app deploy pwa.yaml --quiet failed');
     shell.exit(1);
   }
 }
 
 const srcDir = './';
-const buildDir = './build/custom-es5-bundled';
+const traditionalBuildDir = './build/custom-es5-bundled';
+const progressiveBuildDir = './build/custom-es6-service-worker';
 const rootElementPath = 'src/torque-app.html'
 
 updateBuildNumber(srcDir, rootElementPath);
 runPolymerBuild();
-hashLinks(buildDir);
+hashLinks(traditionalBuildDir);
+publishPwa();
