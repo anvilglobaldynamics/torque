a8222938eebb953718ec1d025da699d55166f7cc
Mujtaba Nowshad <mujtabanowshad@gmail.com>
2018-04-03 21:01:24 +0600

    customer phone now has regex validation

--- server/src/apis/edit-customer.js
+++ server/src/apis/edit-customer.js
@@ -16,7 +16,7 @@ exports.EditCustomerApi = class extends collectionCommonMixin(Api) {
       customerId: Joi.number().max(999999999999999).required(),
 
       fullName: Joi.string().min(1).max(64).required(),
-      phone: Joi.string().alphanum().min(11).max(14).required()
+      phone: Joi.string().regex(/^[a-z0-9\+]*$/).min(11).max(15).required()
     });
   }
 
--- server/src/collections/customer.js
+++ server/src/collections/customer.js
@@ -13,7 +13,7 @@ exports.CustomerCollection = class extends Collection {
       createdDatetimeStamp: Joi.number().max(999999999999999).required(),
       lastModifiedDatetimeStamp: Joi.number().max(999999999999999).required(),
       fullName: Joi.string().min(1).max(64).required(),
-      phone: Joi.string().alphanum().min(11).max(14).required(),
+      phone: Joi.string().regex(/^[a-z0-9\+]*$/).min(11).max(15).required(),
       organizationId: Joi.number().max(999999999999999).required(),
       balance: Joi.number().max(999999999999999).required(),
       isDeleted: Joi.boolean().required(),
