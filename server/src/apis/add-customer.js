c1cfb754fbbfaa8b76fe7c7f8ae1d93b5224ad47
Mujtaba Nowshad <mujtabanowshad@gmail.com>
2018-04-03 21:10:06 +0600

    number regex wip

--- server/src/apis/add-customer.js
+++ server/src/apis/add-customer.js
@@ -13,7 +13,7 @@ exports.AddCustomerApi = class extends Api {
       organizationId: Joi.number().max(999999999999999).required(),
 
       fullName: Joi.string().min(1).max(64).required(),
-      phone: Joi.string().regex(/^[a-z0-9\+]*$/).min(11).max(15).required(),
+      phone: Joi.string().regex(/^[a-z0-9\+]*$/i).min(11).max(15).required(),
       openingBalance: Joi.number().max(999999999999999).required()
     });
   }
