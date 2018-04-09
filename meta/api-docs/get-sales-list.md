0e611cea23485bbe7488292071fe649dcdf56003
Sayem Shafayet <ishafayet@gmail.com>
2018-04-09 11:53:27 +0600

    Added GetSalesListApi#_includeExtendedInformationIfNeeded

--- meta/api-docs/get-sales-list.md
+++ meta/api-docs/get-sales-list.md
@@ -17,7 +17,9 @@ method: `POST`
   shouldFilterByCustomer: Joi.boolean().required(),
   
   fromDate: Joi.number().max(999999999999999).required(),
-  toDate: Joi.number().max(999999999999999).required()
+  toDate: Joi.number().max(999999999999999).required(),
+
+  includeExtendedInformation: Joi.boolean().optional()
 }
 ```
 
@@ -41,7 +43,7 @@ Possible Error Codes:
 { code: ORGANIZATION_INVALID } // organization could not be found
 ```
 
-### response (on success):
+### response (on success, without includeExtendedInformation):
 ```js
 {
   "hasError": false,
@@ -74,6 +76,47 @@ Possible Error Codes:
         paidAmount: Joi.number().max(999999999999999).required(),
         changeAmount: Joi.number().max(999999999999999).required()
       }),
+      isModified: Joi.boolean().required()
+    });
+  )
+}
+```
+
+### response (on success, includeExtendedInformation):
+```js
+{
+  "hasError": false,
+
+  "salesList": Joi.array().items(
+    Joi.object().keys({
+      createdDatetimeStamp: Joi.number().max(999999999999999).required(),
+      lastModifiedDatetimeStamp: Joi.number().max(999999999999999).required(),
+      lastModifiedByUserId: Joi.number().max(999999999999999).required(),
+      outletId: Joi.number().max(999999999999999).required(),
+      customerId: Joi.number().max(999999999999999).required(),
+      productList: Joi.array().items(
+        Joi.object().keys({
+          productId: Joi.number().max(999999999999999).required(),
+          count: Joi.number().max(999999999999999).required(),
+          discountType: Joi.string().max(1024).required(),
+          discountValue: Joi.number().max(999999999999999).required(),
+          salePrice: Joi.number().max(999999999999999).required(),
+          productCategory: Joi.object().required()
+        })
+      ),
+      payment: Joi.object().keys({
+        totalAmount: Joi.number().max(999999999999999).required(),
+        vatAmount: Joi.number().max(999999999999999).required(),
+        discountType: Joi.string().max(1024).required(),
+        discountValue: Joi.number().max(999999999999999).required(),
+        discountedAmount: Joi.number().max(999999999999999).required(),
+        serviceChargeAmount: Joi.number().max(999999999999999).required(),
+        totalBilled: Joi.number().max(999999999999999).required(),
+        previousCustomerBalance: Joi.number().max(999999999999999).required(),
+        paidAmount: Joi.number().max(999999999999999).required(),
+        changeAmount: Joi.number().max(999999999999999).required()
+      }),
+      customer: Joi.object().optional(),
       isModified: Joi.boolean().required()
     });
   )
--- server/src/apis/get-sales-list.js
+++ server/src/apis/get-sales-list.js
@@ -26,7 +26,9 @@ exports.GetSalesListApi = class extends salesCommonMixin(outletCommonMixin(custo
       shouldFilterByCustomer: Joi.boolean().required(),
 
       fromDate: Joi.number().max(999999999999999).required(),
-      toDate: Joi.number().max(999999999999999).required()
+      toDate: Joi.number().max(999999999999999).required(),
+
+      includeExtendedInformation: Joi.boolean().optional()
     });
   }
 
@@ -49,9 +51,57 @@ exports.GetSalesListApi = class extends salesCommonMixin(outletCommonMixin(custo
     this._verifyCustomerExist({ customerId }, cbfn);
   }
 
+  _includeExtendedInformationIfNeeded(salesList, includeExtendedInformation, cbfn) {
+    if (!includeExtendedInformation) return cbfn();
+    Promise.all(salesList.map(sales => {
+      let customerPromise = new Promise((accept, reject) => {
+        if (!sales.customerId) {
+          return accept();
+        }
+        this.database.customer.findById({ customerId: sales.customerId }, (err, customer) => {
+          if (err) return reject(err);
+          if (!this._ensureDoc(err, customer, "CUSTOMER_INVALID", "Customer not found.")) return;
+          sales.customer = customer;
+          return accept();
+        });
+      });
+      let productCategoryPromise = new Promise((accept, reject) => {
+        let productIdList = sales.productList.map(product => product.productId);
+        this.database.product.findByIdList({ idList: productIdList }, (err, productList) => {
+          if (err) return reject(err);
+          if (productList.length !== productList.length) {
+            err = new Error("Unable to find all products in productList");
+            err.code = "PRODUCT_INVALID";
+            return reject(err);
+          }
+          let productCategoryIdList = productList.map(productCategory => productCategory.id);
+          this.database.productCategory.listByIdList({ idList: productCategoryIdList }, (err, productCategoryList) => {
+            if (err) return reject(err);
+            for (let productCategory of productCategoryList) {
+              if (productCategory.isReturnable == false) {
+                err = new Error("product in list is non-returnable");
+                err.code = "PRODUCT_CATEGORY_NON_RETURNABLE";
+                return reject(err);
+              }
+            }
+            productCategoryList.forEach(productCategory => {
+              let _product = productList.find(product => product.productCategoryId === productCategory.id);
+              let product = sales.productList.find(product => product.productId === _product.id);
+              product.productCategory = productCategory;
+            })
+            return accept();
+          });
+        });
+      });
+      return Promise.all([customerPromise, productCategoryPromise]);
+    }))
+      .then(() => cbfn())
+      .catch((ex) => this.fail(ex));
+  }
+
   handle({ body }) {
-    let { organizationId, outletId, customerId, shouldFilterByOutlet, shouldFilterByCustomer, fromDate, toDate } = body;
-    
+    let { organizationId, outletId, customerId, shouldFilterByOutlet, shouldFilterByCustomer, fromDate, toDate, includeExtendedInformation } = body;
+
     toDate = new Date(toDate);
     toDate.setHours(23);
     toDate.setMinutes(59);
@@ -60,7 +110,9 @@ exports.GetSalesListApi = class extends salesCommonMixin(outletCommonMixin(custo
     this._verifyOutletIfNeeded({ outletId, shouldFilterByOutlet }, () => {
       this._verifyCustomerIfNeeded({ customerId, shouldFilterByCustomer }, () => {
         this._getSalesList({ organizationId, outletId, customerId, shouldFilterByOutlet, shouldFilterByCustomer, fromDate, toDate }, (salesList) => {
-          this.success({ salesList });
+          this._includeExtendedInformationIfNeeded(salesList, includeExtendedInformation, () => {
+            this.success({ salesList });
+          });
         });
       });
     });
