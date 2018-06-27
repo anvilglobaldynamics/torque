3f17173c6df0026cd4e2462a4ac929287a1b5174
Sayem Shafayet <ishafayet@gmail.com>
2018-06-28 01:44:24 +0600

    Added EmploymentCollection

---
+++ server/src/collections/employment.js
@@ -0,0 +1,187 @@
+
+const { Collection } = require('./../collection-base');
+const Joi = require('joi');
+
+exports.EmploymentCollection = class extends Collection {
+
+  get name() { return 'employment'; }
+
+  get joiSchema() {
+    return Joi.object().keys({
+      createdDatetimeStamp: Joi.number().max(999999999999999).required(),
+      lastModifiedDatetimeStamp: Joi.number().max(999999999999999).required(),
+      userId: Joi.number().max(999999999999999).required(),
+      organizationId: Joi.number().max(999999999999999).required(),
+      designation: Joi.string().max(1024).required(),
+      role: Joi.string().max(1024).required(),
+      companyProvidedId: Joi.string().alphanum().allow('').required(),
+      privileges: Joi.object().required().keys({
+        PRIV_VIEW_USERS: Joi.boolean().required(),
+        PRIV_MODIFY_USERS: Joi.boolean().required(),
+        PRIV_ADD_USER: Joi.boolean().required(),
+        PRIV_MAKE_USER_AN_OWNER: Joi.boolean().required(),
+        PRIV_MODIFY_USER_PRIVILEGES: Joi.boolean().required(),
+
+        PRIV_ACCESS_POS: Joi.boolean().required(),
+        PRIV_VIEW_SALES: Joi.boolean().required(),
+        PRIV_MODIFY_SALES: Joi.boolean().required(),
+        PRIV_ALLOW_FLAT_DISCOUNT: Joi.boolean().required(),
+        PRIV_ALLOW_INDIVIDUAL_DISCOUNT: Joi.boolean().required(),
+        PRIV_ALLOW_FOC: Joi.boolean().required(),
+
+        PRIV_VIEW_SALES_RETURN: Joi.boolean().required(),
+        PRIV_MODIFY_SALES_RETURN: Joi.boolean().required(),
+
+        PRIV_VIEW_ALL_INVENTORIES: Joi.boolean().required(),
+        PRIV_MODIFY_ALL_INVENTORIES: Joi.boolean().required(),
+        PRIV_TRANSFER_ALL_INVENTORIES: Joi.boolean().required(),
+        PRIV_REPORT_DAMAGES_IN_ALL_INVENTORIES: Joi.boolean().required(),
+
+        PRIV_VIEW_ALL_OUTLETS: Joi.boolean().required(),
+        PRIV_MODIFY_ALL_OUTLETS: Joi.boolean().required(),
+
+        PRIV_VIEW_ALL_WAREHOUSES: Joi.boolean().required(),
+        PRIV_MODIFY_ALL_WAREHOUSES: Joi.boolean().required(),
+
+        PRIV_VIEW_ORGANIZATION_STATISTICS: Joi.boolean().required(),
+        PRIV_MODIFY_ORGANIZATION: Joi.boolean().required(),
+
+        PRIV_VIEW_CUSTOMER: Joi.boolean().required(),
+        PRIV_ADD_CUSTOMER_DURING_SALES: Joi.boolean().required(),
+        PRIV_MODIFY_CUSTOMER: Joi.boolean().required(),
+        PRIV_MANAGE_CUSTOMER_DEBT: Joi.boolean().required()
+      }),
+      isActive: Joi.boolean().required()
+    });
+  }
+
+  get uniqueKeyDefList() {
+    return [];
+  }
+
+  get foreignKeyDefList() {
+    return [
+      {
+        targetCollection: 'organization',
+        foreignKey: 'id',
+        referringKey: 'organizationId'
+      },
+      {
+        targetCollection: 'user',
+        foreignKey: 'id',
+        referringKey: 'userId'
+      }
+    ];
+  }
+
+  get __fullPrivilegeList() {
+    return [
+      "PRIV_VIEW_USERS",
+      "PRIV_MODIFY_USERS",
+      "PRIV_ADD_USER",
+      "PRIV_MAKE_USER_AN_OWNER",
+      "PRIV_MODIFY_USER_PRIVILEGES",
+
+      "PRIV_ACCESS_POS",
+      "PRIV_VIEW_SALES",
+      "PRIV_MODIFY_SALES",
+      "PRIV_ALLOW_FLAT_DISCOUNT",
+      "PRIV_ALLOW_INDIVIDUAL_DISCOUNT",
+      "PRIV_ALLOW_FOC",
+
+      "PRIV_VIEW_SALES_RETURN",
+      "PRIV_MODIFY_SALES_RETURN",
+
+      "PRIV_VIEW_ALL_INVENTORIES",
+      "PRIV_MODIFY_ALL_INVENTORIES",
+      "PRIV_TRANSFER_ALL_INVENTORIES",
+      "PRIV_REPORT_DAMAGES_IN_ALL_INVENTORIES",
+
+      "PRIV_VIEW_ALL_OUTLETS",
+      "PRIV_MODIFY_ALL_OUTLETS",
+
+      "PRIV_VIEW_ALL_WAREHOUSES",
+      "PRIV_MODIFY_ALL_WAREHOUSES",
+
+      "PRIV_VIEW_ORGANIZATION_STATISTICS",
+      "PRIV_MODIFY_ORGANIZATION",
+
+      "PRIV_VIEW_CUSTOMER",
+      "PRIV_ADD_CUSTOMER_DURING_SALES",
+      "PRIV_MODIFY_CUSTOMER",
+      "PRIV_MANAGE_CUSTOMER_DEBT"
+    ];
+  }
+
+  __generatePrivilegeModel(allowedPrivilegeList) {
+    let privileges = {};
+    this.__fullPrivilegeList.forEach(privilege => {
+      if (allowedPrivilegeList.indexOf(privilege) > -1) {
+        privileges[privilege] = true;
+      } else {
+        privileges[privilege] = false;
+      }
+    })
+  }
+
+  async addOwner({ organizationId, userId }) {
+    return await this._insert({
+      createdDatetimeStamp: (new Date).getTime(),
+      lastModifiedDatetimeStamp: (new Date).getTime(),
+      userId,
+      organizationId,
+      designation: 'Owner',
+      role: 'owner',
+      companyProvidedId: '',
+      privileges: this.__generatePrivilegeModel(this.__fullPrivilegeList),
+      isActive: true
+    });
+  }
+
+  async addRegularEmployee({ userId, organizationId, role, designation, companyProvidedId, privileges }) {
+    return await this._insert({
+      createdDatetimeStamp: (new Date).getTime(),
+      lastModifiedDatetimeStamp: (new Date).getTime(),
+      userId,
+      organizationId,
+      designation,
+      role,
+      companyProvidedId,
+      privileges,
+      isActive: true
+    });
+  }
+
+  async listByOrganizationId({ organizationId }) {
+    return await this._find({ organizationId });
+  }
+
+  async getEmploymentOfUserInOrganization({ userId, organizationId }) {
+    return await this._find({ userId, organizationId });
+  }
+
+  async getEmploymentsOfUser({ userId }) {
+    return await this._find({ userId });
+  }
+
+  async getActiveEmploymentsOfUser({ userId }) {
+    return await this._find({ userId, isActive: true });
+  }
+
+  async setActiveStatus({ id }, { isActive }) {
+    return await this._update({ id }, {
+      $set: {
+        isActive
+      }
+    });
+  }
+
+  async setDetails({ id }, { role, designation, companyProvidedId, privileges }) {
+    return await this._update({ id }, {
+      $set: {
+        role, designation, companyProvidedId, privileges
+      }
+    });
+  }
+
+}
