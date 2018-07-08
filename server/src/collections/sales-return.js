8ebd56bc9242bb2e89bb01710888a86d1317a0c3
Sayem Shafayet <ishafayet@gmail.com>
2018-07-08 13:41:21 +0600

    Added SalesReturnCollection

---
+++ server/src/collections/sales-return.js
@@ -0,0 +1,85 @@
+
+const { Collection } = require('./../collection-base');
+const Joi = require('joi');
+
+exports.SalesReturnCollection = class extends Collection {
+
+  get name() { return 'sales-return'; }
+
+  get joiSchema() {
+    return Joi.object().keys({
+      createdDatetimeStamp: Joi.number().max(999999999999999).required(),
+
+      salesId: Joi.number().max(999999999999999).required(),
+      returnedProductList: Joi.array().items(
+        Joi.object().keys({
+          productId: Joi.number().max(999999999999999).required(),
+          count: Joi.number().max(999999999999999).required()
+        })
+      ),
+      creditedAmount: Joi.number().max(999999999999999).required(),
+
+      isDeleted: Joi.boolean().required()
+    });
+  }
+
+  get uniqueKeyDefList() {
+    return [];
+  }
+
+  get foreignKeyDefList() {
+    return [
+      {
+        targetCollection: 'sales',
+        foreignKey: 'id',
+        referringKey: 'salesId'
+      }
+    ];
+  }
+
+  // NOTE: commented out, because currently we don't support deleting sales return.
+  // get deletionIndicatorKey() { return 'isDeleted'; }
+
+  async create({ salesId, returnedProductList, creditedAmount }) {
+    return await this._insert({
+      createdDatetimeStamp: (new Date).getTime(),
+
+      salesId,
+      returnedProductList,
+      creditedAmount,
+
+      isDeleted: false
+    });
+  }
+
+  async listBySalesIdList({ salesIdList }) {
+    return await this._find({
+      salesId: { $in: salesIdList }
+    });
+  }
+
+  async listBySalesId({ listBySalesIdList }) {
+    return await this._find({
+      salesId
+    });
+  }
+
+  async listByFilters({ salesIdList, fromDate, toDate }) {
+    let query = {
+      $and: [
+        {
+          salesId: { $in: salesIdList }
+        },
+        {
+          createdDatetimeStamp: {
+            $gte: fromDate,
+            $lte: toDate
+          }
+        }
+      ]
+    };
+
+    return await this._find(query);
+  }
+  
+}
