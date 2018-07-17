e8538c2f268bb72b6395b1a63fca6027b99281d6
Sayem Shafayet <ishafayet@gmail.com>
2018-07-18 00:52:38 +0600

    Most likely resolves partial credit or hybrid sale vulnerability #272

--- server/src/legacy-apis/add-sales.js
+++ server/src/legacy-apis/add-sales.js
@@ -81,19 +81,22 @@ exports.AddSalesApi = class extends inventoryCommonMixin(customerCommonMixin(col
 
   _handlePayment({ payment, customer }, cbfn) {
     let diff = (payment.paidAmount + payment.previousCustomerBalance) - payment.totalBilled;
-
-    if (diff >= 0) {
-      return cbfn(payment);
-    } else {
-      if (!customer) {
-        let err = new Error("credit sale is not allowed without registered cutomer");
-        err.code = "CREDIT_SALE_NOT_ALLOWED_WITHOUT_CUSTOMER";
-        return this.fail(err);
-      }
-      this._updateCustomerBalance({ diff, customer }, () => {
-        return cbfn(payment);
-      });
+    if (diff < 0 && !customer) {
+      let err = new Error("credit sale is not allowed without registered cutomer");
+      err.code = "CREDIT_SALE_NOT_ALLOWED_WITHOUT_CUSTOMER";
+      return this.fail(err);
     }
+    if (diff < 0 && !payment.allowCreditSale) {
+      let err = new Error("You have not permitted credit sales.");
+      err.code = "CREDIT_SALE_NOT_PERMITTED_BY_SALES_PERSON";
+      return this.fail(err);
+    }
+    if (diff > 0 && !payment.shouldSaveChangeInAccount) {
+      diff = 0; // because the change amount is not saved to user's account any more.
+    }
+    this._updateCustomerBalance({ diff, customer }, () => {
+      return cbfn(payment);
+    });
   }
 
   _addSales({ outletId, customerId, productList, payment }, cbfn) {
