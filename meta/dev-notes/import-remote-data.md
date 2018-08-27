0758e95cf03423d337456d289e5aef3c005097b9
Sayem Shafayet <ishafayet@gmail.com>
2018-08-27 14:31:21 +0600

    Added support for importing remote database #328

---
+++ meta/dev-notes/import-remote-data.md
@@ -0,0 +1,4 @@
+
+Run `npm run import`. You will need remote db password.
+
+Run `npm run dev-imported`.
--- server/src/utils/command-line.js
+++ server/src/utils/command-line.js
@@ -1,6 +1,6 @@
 
 exports.parseCommandLineParameters = _ => {
- 
+
   let params = {};
 
   if (!process.argv.some(str => str.includes('_mocha'))) {
@@ -9,16 +9,19 @@ exports.parseCommandLineParameters = _ => {
       .version('0.1.0')
       .option('-p, --dry-run', 'Runs the startup process without starting server.')
       .option('-P, --validate', 'Check database integrity')
+      .option('-P, --db <db>', 'Override db to use')
       .parse(process.argv);
   }
 
   let {
     validate = false,
-    dryRun = false
+    dryRun = false,
+    db = null,
   } = params;
 
   return {
     validate,
-    dryRun
+    dryRun,
+    db
   };
 }
