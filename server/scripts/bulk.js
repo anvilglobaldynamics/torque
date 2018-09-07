7aec6b963f42066ab6db0458a6326a58cf726dd3
Sayem Shafayet <ishafayet@gmail.com>
2018-09-07 20:07:17 +0600

    Added scaffolding

--- server/scripts/bulk.js
+++ server/scripts/bulk.js
@@ -7,16 +7,16 @@ const fslib = require('fs-extra');
 const phonePrefix = '998';
 const commonPassword = '12345678';
 
-const organizationCount = 50;
+const organizationCount = 4;
 const employeeCount = {
   min: 0,
   max: 2
 };
-const warehouse = {
+const warehouseCount = {
   min: 3,
   max: 20
 };
-const outlet = {
+const outletCount = {
   min: 3,
   max: 40
 };
@@ -46,4 +46,56 @@ const nounList = _loadDataFragment('nouns');
 const pickOne = (list) => {
   return list[Math.floor(Math.random() * list.length)];
 }
+
+const createUser = async () => {
+  console.log('should create user');
+}
+
+const createOrganization = async () => {
+  console.log('should create organization');
+}
+
+const createProductCategory = async () => {
+  console.log('should create productCategory');
+}
+
+const createWarehouse = async () => {
+  console.log('should create warehouse');
+}
+
+const createOutlet = async () => {
+  console.log('should create outlet');
+}
+
+const createProduct = async () => {
+  console.log('should create product');
+}
+
+const generateBulkData = async () => {
+  let owner = await createUser();
+
+  for (let i = 0; i < getSolidCount(organizationCount); i++) {
+    let organization = await createOrganization();
+
+    for (let i = 0; i < getSolidCount(employeeCount); i++) {
+      let employee = await createOrganization();
+    }
+
+    for (let i = 0; i < getSolidCount(warehouseCount); i++) {
+      let warehouse = await createWarehouse();
+    }
+
+    for (let i = 0; i < getSolidCount(outletCount); i++) {
+      let outlet = await createOutlet();
+    }
+
+    for (let i = 0; i < getSolidCount(productCategoryCount); i++) {
+      let productCategory = await createProductCategory();
+    }
+
+  }
+
+}
+
+generateBulkData();
 
