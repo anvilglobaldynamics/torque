3f3bec30fb7d8b6680f8b4c29ac3e93f62abe2f5
Sayem Shafayet <ishafayet@gmail.com>
2018-09-15 19:44:54 +0600

    Removed skipped tests regarding previously removed unique key / duplication checks and moved to legacy/fragments dir

--- legacy/fragments/removed-tests.js
+++ legacy/fragments/removed-tests.js
@@ -119,3 +119,157 @@ it.skip('api/delete-product-category (Invalid parent deletetion)', testDoneFn =>
   })
 
 });
+
+// from test-organization-apis.js =
+
+it.skip('api/add-organization (Invalid, copy phone)', testDoneFn => {
+
+  callApi('api/add-organization', {
+    json: {
+      apiKey,
+      name: "My Organization",
+      primaryBusinessAddress: "My Address",
+      phone: orgPhone,
+      email: org2Email
+    }
+  }, (err, response, body) => {
+    expect(response.statusCode).to.equal(200);
+    expect(body).to.have.property('hasError').that.equals(true);
+    expect(body).to.have.property('error');
+    expect(body.error).to.have.property('code').that.equals('PHONE_ALREADY_IN_USE');
+    testDoneFn();
+  })
+
+});
+
+it.skip('api/add-organization (Invalid, copy email)', testDoneFn => {
+
+  callApi('api/add-organization', {
+    json: {
+      apiKey,
+      name: "My Organization",
+      primaryBusinessAddress: "My Address",
+      phone: org2Phone,
+      email: orgEmail
+    }
+  }, (err, response, body) => {
+    expect(response.statusCode).to.equal(200);
+    expect(body).to.have.property('hasError').that.equals(true);
+    expect(body).to.have.property('error');
+    expect(body.error).to.have.property('code').that.equals('EMAIL_ALREADY_IN_USE');
+    testDoneFn();
+  })
+
+});
+
+
+it.skip('api/edit-organization (Invalid, copy phone)', testDoneFn => {
+
+  callApi('api/edit-organization', {
+    json: {
+      apiKey,
+      organizationId: organizationToBeEdited.id,
+      name: organizationToBeEdited.name,
+      primaryBusinessAddress: organizationToBeEdited.primaryBusinessAddress,
+      phone: org2Phone,
+      email: organizationToBeEdited.email
+    }
+  }, (err, response, body) => {
+    expect(response.statusCode).to.equal(200);
+    expect(body).to.have.property('hasError').that.equals(true);
+    expect(body).to.have.property('error');
+    expect(body.error).to.have.property('code').that.equals('PHONE_ALREADY_IN_USE');
+    testDoneFn();
+  });
+
+});
+
+it.skip('api/edit-organization (Invalid, copy email)', testDoneFn => {
+
+  callApi('api/edit-organization', {
+    json: {
+      apiKey,
+      organizationId: organizationToBeEdited.id,
+      name: organizationToBeEdited.name,
+      primaryBusinessAddress: organizationToBeEdited.primaryBusinessAddress,
+      phone: organizationToBeEdited.phone,
+      email: org2Email
+    }
+  }, (err, response, body) => {
+    expect(response.statusCode).to.equal(200);
+    expect(body).to.have.property('hasError').that.equals(true);
+    expect(body).to.have.property('error');
+    expect(body.error).to.have.property('code').that.equals('EMAIL_ALREADY_IN_USE');
+    testDoneFn();
+  });
+
+});
+
+// from test-outlet-apis.js =
+
+it.skip('api/add-outlet (Invalid, copy phone)', testDoneFn => {
+
+  callApi('api/add-outlet', {
+    json: {
+      apiKey,
+      organizationId,
+      name: "My Outlet",
+      physicalAddress: "batcave address",
+      phone: outletPhone,
+      contactPersonName: "test contact person name"
+    }
+  }, (err, response, body) => {
+    expect(response.statusCode).to.equal(200);
+    expect(body).to.have.property('hasError').that.equals(true);
+    expect(body).to.have.property('error');
+    expect(body.error).to.have.property('code').that.equals('PHONE_ALREADY_IN_USE');
+    testDoneFn();
+  })
+
+});
+
+// from test-warehouse-apis.js =
+
+it.skip('api/add-warehouse (Invalid copy phone)', testDoneFn => {
+
+  callApi('api/add-warehouse', {
+    json: {
+      apiKey,
+      organizationId,
+      name: "My Warehouse",
+      physicalAddress: "wayne manor address",
+      phone: warehousePhone,
+      contactPersonName: "test contact person name"
+    }
+  }, (err, response, body) => {
+    expect(response.statusCode).to.equal(200);
+    expect(body).to.have.property('hasError').that.equals(true);
+    expect(body).to.have.property('error');
+    expect(body.error.code).to.equal('PHONE_ALREADY_IN_USE');
+
+    testDoneFn();
+  })
+
+});
+
+
+it.skip('api/edit-warehouse (Invalid copy phone)', testDoneFn => {
+
+  callApi('api/edit-warehouse', {
+    json: {
+      apiKey,
+      warehouseId: warehouseToBeModified.id,
+
+      name: "My Warehouse",
+      physicalAddress: "wayne manor address",
+      phone: warehousePhone2,
+      contactPersonName: "test contact person name"
+    }
+  }, (err, response, body) => {
+    expect(response.statusCode).to.equal(200);
+    validateGenericApiFailureResponse(body);
+    expect(body.error.code).equal('PHONE_ALREADY_IN_USE');
+    testDoneFn();
+  })
+
+});
