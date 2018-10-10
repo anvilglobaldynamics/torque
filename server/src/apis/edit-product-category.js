3f042d6898046335c168911b6af043230f7b87e1
iLGunners <mujtabanowshad@gmail.com>
2018-10-10 13:01:41 +0600

    product categories related checks for 412

--- server/src/apis/edit-product-category.js
+++ server/src/apis/edit-product-category.js
@@ -2,8 +2,9 @@ const { Api } = require('./../api-base');
 const Joi = require('joi');
 const { throwOnFalsy, throwOnTruthy, CodedError } = require('./../utils/coded-error');
 const { extract } = require('./../utils/extract');
+const { ProductCategoryMixin } = require('./mixins/product-category-mixin');
 
-exports.EditProductCategoryApi = class extends Api {
+exports.EditProductCategoryApi = class extends Api.mixin(ProductCategoryMixin) {
 
   get autoValidates() { return true; }
 
@@ -42,13 +43,6 @@ exports.EditProductCategoryApi = class extends Api {
         "PRIV_MODIFY_ALL_INVENTORIES"
       ]
     }];
-  }
-
-  async _checkIfDiscountValueIsValid({ defaultDiscountType, defaultDiscountValue, defaultSalePrice, defaultVat }) {
-    let salePriceAfterVat = defaultSalePrice + defaultSalePrice * defaultVat / 100;
-    if (defaultDiscountValue && defaultDiscountType === 'fixed' && defaultDiscountValue > salePriceAfterVat) {
-      throw new CodedError("DISCOUNT_VALUE_INVALID", "the discount value is more than sale price");
-    }
   }
 
   async _updateProductCategory({ productCategoryId, name, unit, defaultDiscountType, defaultDiscountValue, defaultPurchasePrice, defaultVat, defaultSalePrice, isReturnable }) {
