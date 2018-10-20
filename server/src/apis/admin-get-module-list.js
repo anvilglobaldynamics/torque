9277715f3639aba98b9997564b9a7513f4269678
Sayem Shafayet <ishafayet@gmail.com>
2018-10-20 13:32:41 +0600

    Added AdminGetModuleListApi

---
+++ server/src/apis/admin-get-module-list.js
@@ -0,0 +1,25 @@
+
+const { Api } = require('./../api-base');
+const Joi = require('joi');
+const { throwOnFalsy, throwOnTruthy, CodedError } = require('./../utils/coded-error');
+
+exports.AdminGetModuleListApi = class extends Api {
+
+  get autoValidates() { return true; }
+
+  get requiresAuthentication() { return true; }
+
+  get authenticationLevel() { return 'admin'; }
+
+  get requestSchema() {
+    return Joi.object().keys({
+      apiKey: Joi.string().length(64).required()
+    });
+  }
+
+  async handle({ body }) {
+    let moduleList = await this.database.fixture.getModuleList();
+    return { moduleList };
+  }
+
+}
