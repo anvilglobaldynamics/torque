1b385aaa7ddb57ecc668d780f72f274e5656d5cc
iLGunners <mujtabanowshad@gmail.com>
2018-10-20 11:31:41 +0600

    Service Blueprint test base added

---
+++ server/test/test-service-blueprint-apis.js
@@ -0,0 +1,58 @@
+let expect = require('chai').expect;
+
+let { callApi } = require('./utils');
+let {
+  rnd,
+  generateInvalidId,
+  initializeServer,
+  terminateServer,
+  registerUser,
+  loginUser,
+  addOrganization
+} = require('./lib');
+
+const prefix = 's';
+
+const password = "123545678";
+const fullName = "Test User";
+const phone = rnd(prefix, 11);
+
+const orgEmail = 'o' + `${rnd(prefix)}@gmail.com`;
+const orgName = "Test Organization";
+const orgBusinessAddress = "Test Organization Address";
+const orgPhone = 'o' + rnd(prefix, 11);
+
+let apiKey = null;
+let organizationId = null;
+
+describe.only('Service Blueprint', _ => {
+
+  it('START', testDoneFn => {
+    initializeServer(_ => {
+      registerUser({
+        password, fullName, phone
+      }, _ => {
+        loginUser({
+          emailOrPhone: phone, password
+        }, (data) => {
+          apiKey = data.apiKey;
+          addOrganization({
+            apiKey,
+            name: orgName,
+            primaryBusinessAddress: orgBusinessAddress,
+            phone: orgPhone,
+            email: orgEmail
+          }, (data) => {
+            organizationId = data.organizationId;
+            testDoneFn();
+          })
+        });
+      });
+    });
+  });
+
+  it('END', testDoneFn => {
+    terminateServer(testDoneFn);
+  });
+
+});
