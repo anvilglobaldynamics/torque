a536f1bdb28f4240f3f672d52f6d1eb3de8d1ebf
Sayem Shafayet <ishafayet@gmail.com>
2018-10-23 23:43:22 +0600

    Added AdminListOrganizationModulesApi

---
+++ server/src/apis/admin-list-organization-modules.js
@@ -0,0 +1,44 @@
+
+const { Api } = require('./../api-base');
+const Joi = require('joi');
+const { throwOnFalsy, throwOnTruthy, CodedError } = require('./../utils/coded-error');
+
+exports.AdminListOrganizationModulesApi = class extends Api {
+
+  get autoValidates() { return true; }
+
+  get requiresAuthentication() { return true; }
+
+  get authenticationLevel() { return 'admin'; }
+
+  get requestSchema() {
+    return Joi.object().keys({
+      apiKey: Joi.string().length(64).required(),
+      organizationId: Joi.number().max(999999999999999).required(),
+    });
+  }
+
+  async _insertModuleDetailsInModuleActivationList({ moduleActivationList }) {
+    let moduleList = await this.database.fixture.getModuleList();
+    moduleActivationList.forEach(moduleActivation => {
+      moduleList.forEach(aModule => {
+        if (aModule.code == moduleActivation.packageCode) {
+          moduleActivation.packageDetail = aModule;
+        }
+      });
+    });
+    return moduleActivationList;
+  }
+
+  async handle({ body }) {
+    let { organizationId } = body;
+    let organization = await this.database.organization.findById({ id: organizationId });
+    throwOnFalsy(organization, "ORGANIZATION_DOES_NOT_EXIST", this.verses.organizationCommon.organizationDoesNotExist);
+
+    let moduleActivationList = await this.database.moduleActivation.listByOrganizationId({ organizationId });
+    moduleActivationList = await this._insertModuleDetailsInModuleActivationList({ moduleActivationList });
+
+    return { moduleActivationList };
+  }
+
+}
