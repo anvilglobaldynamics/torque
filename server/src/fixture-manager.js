77a454801e5441b0fe301a6849384d220a4c350d
Sayem Shafayet <ishafayet@gmail.com>
2018-11-05 16:45:23 +0600

    Fixed small typo and increased fixture manager version

--- server/src/fixture-manager.js
+++ server/src/fixture-manager.js
@@ -28,7 +28,7 @@ class FixtureManager {
       {
         name: "privilege-list",
         path: "./src/fixtures/privilege-list.json",
-        version: 7
+        version: 8
       },
       {
         name: "package-list",
