5056980877065345901d5a9fa8969d0b3b3e9dc4
Sayem Shafayet <ishafayet@gmail.com>
2019-05-05 11:07:46 +0600

    Added methods for calling apis

--- server/scripts/bulk/bulk-stress.js
+++ server/scripts/bulk/bulk-stress.js
@@ -21,10 +21,102 @@ let params = {
   customerCount: 50
 };
 
+const _getAggregatedInventoryDetails = async (apiKey, inventoryId) => {
+
+  // console.log('should add product to outlet', i);
+
+  let results = await callApi('api/get-aggregated-inventory-details', {
+    apiKey,
+    inventoryId
+  });
+
+  return {};
+
+}
+
+const _bulkImportProductBlueprint = async (apiKey, organizationId) => {
+
+  // console.log('should add product to outlet', i);
+
+  let results = await callApi('api/bulk-import-product-blueprints', {
+    apiKey,
+    organizationId,
+    rowList: [
+      ["Should Be Unique 1", "pc", 300, 500, 10, "Yes", ''],
+      ["Should Be Unique 2", "haali", 10, 10, 10, "No", '']
+    ]
+  });
+
+  return {};
+
+}
+
+const _getSalesList = async (apiKey, organizationId) => {
+
+  // console.log('should add product to outlet', i);
+
+  let results = await callApi('api/get-sales-list', {
+    apiKey,
+    organizationId,
+    outletId: null,
+    customerId: null,
+
+    shouldFilterByOutlet: false,
+    shouldFilterByCustomer: false,
+
+    fromDate,
+    toDate: (new Date()).getTime(),
+    includeExtendedInformation: true,
+
+    // searchString: String(salesId)
+  });
+
+  return {};
+
+}
+
+const _getServiceMembershipList = async (apiKey, inventoryId) => {
+
+  console.log('should add product to outlet', i);
+
+  let results = await callApi('api/get-service-membership-list', {
+    apiKey,
+    organizationId,
+    serviceBlueprintId: null,
+    outletId: null,
+    customerId: null,
+
+    shouldFilterByServiceBlueprint: false,
+    shouldFilterByOutlet: false,
+    shouldFilterByCustomer: false,
+
+    fromDate: monthsEarlierDate,
+    toDate: monthsLaterDate
+  });
+
+  return {};
+
+}
+
+
+// const _getAggregatedInventoryDetails = async (apiKey, inventoryId) => {
+
+//   console.log('should add product to outlet', i);
+
+//   let results = await callApi('api/get-aggregated-inventory-details', {
+//     apiKey,
+//     inventoryId
+//   });
+
+//   return {};
+
+// }
+
+
+
 (async () => {
   let { apiKey, primaryUserPhone, commonPassword, reusables } = await generateBulkData(params);
   console.log({ apiKey, primaryUserPhone, commonPassword, reusables });
-
 
 
 
