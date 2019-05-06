55eb61fe2422ea56ca0fbcb0153914e54e7c1434
Sayem Shafayet <ishafayet@gmail.com>
2019-05-06 12:15:03 +0600

    Switched to live server

--- server/scripts/bulk-core.js
+++ server/scripts/bulk-core.js
@@ -90,7 +90,7 @@ let uid = 0;
 let request = require('request');
 
 let genUrl = exports.genUrl = (path) => {
-  return "http://localhost:8540/" + path
+  return "https://server.lipi.live/" + path
 }
 
 _callApi = (...args) => {
