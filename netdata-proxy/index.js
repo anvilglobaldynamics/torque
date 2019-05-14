66b63d864b24240075933b2981d53fb5c9695754
Sayem Shafayet <ishafayet@gmail.com>
2019-05-14 14:31:04 +0600

    Added SSL and config loading support for netdata-proxy

--- netdata-proxy/index.js
+++ netdata-proxy/index.js
@@ -1,32 +1,70 @@
-var http = require('http'),
-  httpProxy = require('http-proxy');
 
-var proxy = httpProxy.createProxyServer({});
+const { ConfigLoader } = require('../server/src/config-loader');
+const https = require('https');
+const fs = require('fs');
+const httpProxy = require('http-proxy');
 
-proxy.on('proxyReq', function (proxyReq, req, res, options) {
-  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
-});
-
-var server = http.createServer(function (req, res) {
-
-  try {
-    const b64auth = ((req.headers.authorization || '').split(' ')[1] || '');
-    const [login, password] = new Buffer(b64auth, 'base64').toString().split(':');
-    if (!(login === 'admin' && password === 'admin')) {
-      throw new Error("Invalid Credentials");
+const _getSslDetails = (config) => {
+  let { key, cert, caBundle } = config.server.ssl;
+  key = fs.readFileSync(key, 'utf8');
+  cert = fs.readFileSync(cert, 'utf8');
+  caBundle = fs.readFileSync(caBundle, 'utf8');
+  let ca = [];
+  let buffer = [];
+  for (let line of caBundle.split('\n')) {
+    buffer.push(line);
+    if (line.indexOf('-END CERTIFICATE-') > -1) {
+      ca.push(buffer.join('\n'));
+      buffer = [];
     }
-    // res.end("ALL OK");
-  } catch (ex) {
-    res.setHeader('WWW-Authenticate', 'Basic realm="401"') // change this
-    res.statusCode = 401;
-    res.end('Authentication required.') // custom message
-    return;
   }
+  return { key, cert, ca };
+}
 
-  proxy.web(req, res, {
-    target: 'http://127.0.0.1:19999'
+
+const _getConfig = () => {
+  return ConfigLoader.getComputedConfig();
+}
+
+
+const main = () => {
+
+  let config = _getConfig();
+
+  let proxy = httpProxy.createProxyServer({});
+
+  proxy.on('proxyReq', function (proxyReq, req, res, options) {
+    proxyReq.setHeader('X-Proxied-By', 'torque');
   });
-});
 
-console.log("listening on port 19998")
-server.listen(19998);
+  let sslDetails = _getSslDetails(config);
+
+  let server = https.createServer(sslDetails, (req, res) => {
+
+    try {
+      const b64auth = ((req.headers.authorization || '').split(' ')[1] || '');
+      const [login, password] = new Buffer(b64auth, 'base64').toString().split(':');
+      if (!(login === 'admin' && password === 'admin')) {
+        throw new Error("Invalid Credentials");
+      }
+    } catch (ex) {
+      res.setHeader('WWW-Authenticate', 'Basic realm="401"') // change this
+      res.statusCode = 401;
+      res.end('Authentication required.') // custom message
+      return;
+    }
+
+    proxy.web(req, res, {
+      target: 'http://127.0.0.1:19999'
+    });
+
+  });
+
+  console.log("(netdata-proxy)> listening on port 19998");
+  server.listen(19998);
+
+}
+
+main();
+
+
