749c47576046f2d7bcb11cd0a7a416e39fab0c86
M Nowshad <mujtaba.nowshad@gmail.com>
2019-06-16 12:13:03 +0600

    privilege message changed

--- server/src/fixture-manager.js
+++ server/src/fixture-manager.js
@@ -28,7 +28,7 @@ class FixtureManager {
       {
         name: "privilege-list",
         path: "./src/fixtures/privilege-list.json",
-        version: 12
+        version: 13
       },
       {
         name: "package-list",
