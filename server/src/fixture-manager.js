8c125868e40a297cd3fbc1abbdf4b9f26919009f
Sayem Shafayet <ishafayet@gmail.com>
2019-07-11 19:47:48 +0600

    Updated package for SNN Imports

--- server/src/fixture-manager.js
+++ server/src/fixture-manager.js
@@ -33,7 +33,7 @@ class FixtureManager {
       {
         name: "package-list",
         path: "./src/fixtures/package-list.json",
-        version: 16
+        version: 17
       },
       {
         name: "module-list",
