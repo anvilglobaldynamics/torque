1ecc50277ad8313654f95d2dbfcf05fc7c12fc33
Sayem Shafayet <ishafayet@gmail.com>
2019-08-09 16:44:42 +0600

    Added devops script for sprint 0.3.7

--- server/scripts/devops/generic-db-ops.js
+++ server/scripts/devops/generic-db-ops.js
@@ -28,16 +28,73 @@ const find = async (collectionName, query) => {
   return await db.collection(collectionName).find(query).sort(sort).toArray();
 }
 
+const updateOne = async (collectionName, query, modifications) => {
+  let results = await db.collection(collectionName).updateOne(query, modifications);
+  return (results.matchedCount === 1);
+}
+
 // ====================================== code
 
-const process = async () => {
+const runCode = async () => {
   await connect(dbConfig);
+
+  // Part 1. Set organizationId
   let productAcquisitionList = await find('product-acquisition', {});
-  console.log(productAcquisitionList)
+
+  for (let productAcquisition of productAcquisitionList) {
+    if ('organizationId' in productAcquisition) continue;
+    let inventory = await find('inventory', { id: productAcquisition.inventoryId });
+    await updateOne('product-acquisition', { id: productAcquisition.id }, {
+      $set: {
+        organizationId: inventory[0].organizationId
+      }
+    });
+    console.log('set organizationId')
+  }
+
+  // Part 2. Set vendorId
+  for (let productAcquisition of productAcquisitionList) {
+    if ('vendorId' in productAcquisition) continue;
+    await updateOne('product-acquisition', { id: productAcquisition.id }, {
+      $set: {
+        vendorId: null
+      }
+    });
+    console.log('set vendorId')
+  }
+
+  // Part 3. set productAcquisitionNumber
+  let organizationList = await find('organization', {});
+  for (let organization of organizationList) {
+    let productAcquisitionNumber = 1;
+    let productAcquisitionList = await find('product-acquisition', { organizationId: organization.id });
+    for (let productAcquisition of productAcquisitionList) {
+      await updateOne('product-acquisition', { id: productAcquisition.id }, {
+        $set: {
+          productAcquisitionNumber
+        }
+      });
+      productAcquisitionNumber += 1;
+      console.log({ productAcquisitionNumber });
+    }
+
+    await updateOne('auto-generated-organization-specific-number', { organizationId: organization.id }, {
+      $set: {
+        productAcquisitionNumberSeed: productAcquisitionNumber
+      }
+    });
+  }
 }
 
 // ====================================== start
 
 const mode = 'dev';
 let dbConfig = config(mode);
-process();
+runCode()
+  .then(() => {
+    process.exit();
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit();
+  });
