0bc94d36397395fa29b36961d0893a7062275215
Sayem Shafayet <ishafayet@gmail.com>
2019-09-07 12:08:01 +0600

    Privileges are now consistently loaded from privilege loader

--- server/src/legacy-apis/hire-user-as-employee.js
+++ server/src/legacy-apis/hire-user-as-employee.js
@@ -1,6 +1,8 @@
 let { LegacyApi } = require('./../legacy-api-base');
 let Joi = require('joi');
 
+const { getPrivilegesSchemaFromJson } = require('../utils/privilege-loader');
+
 let { collectionCommonMixin } = require('./mixins/collection-common');
 let { userCommonMixin } = require('./mixins/user-common');
 
@@ -21,54 +23,7 @@ exports.HireUserAsEmployeeApi = class extends userCommonMixin(collectionCommonMi
       designation: Joi.string().max(64).required(),
       companyProvidedId: Joi.string().allow('').max(64).required(),
 
-      privileges: Joi.object().required().keys({
-        PRIV_VIEW_USERS: Joi.boolean().required(),
-        PRIV_MODIFY_USERS: Joi.boolean().required(),
-
-        PRIV_ACCESS_POS: Joi.boolean().required(),
-        PRIV_VIEW_SALES: Joi.boolean().required(),
-        PRIV_MODIFY_SALES: Joi.boolean().required(),
-        PRIV_ALLOW_FLEXIBLE_PRICE: Joi.boolean().required(),
-        PRIV_VIEW_PURCHASE_PRICE: Joi.boolean().required(),
-
-        PRIV_MODIFY_DISCOUNT_PRESETS: Joi.boolean().required(),
-
-        PRIV_VIEW_ALL_SERVICE_MEMBERSHIPS: Joi.boolean().required(),
-        PRIV_MODIFY_ALL_SERVICE_MEMBERSHIPS: Joi.boolean().required(),
-
-        PRIV_VIEW_SALES_RETURN: Joi.boolean().required(),
-        PRIV_MODIFY_SALES_RETURN: Joi.boolean().required(),
-
-        PRIV_VIEW_ALL_INVENTORIES: Joi.boolean().required(),
-        PRIV_VIEW_ALL_SERVICES: Joi.boolean().required(),
-        PRIV_MODIFY_ALL_PRODUCT_BLUEPRINTS: Joi.boolean().required(),
-        PRIV_MODIFY_ALL_SERVICE_BLUEPRINTS: Joi.boolean().required(),
-        PRIV_VIEW_ALL_SERVICE_BLUEPRINTS: Joi.boolean().required(),
-        PRIV_VIEW_ALL_PRODUCT_BLUEPRINTS: Joi.boolean().required(),
-        PRIV_TRANSFER_ALL_INVENTORIES: Joi.boolean().required(),
-        PRIV_ADD_PRODUCTS_TO_ALL_INVENTORIES: Joi.boolean().required(),
-        PRIV_MODIFY_ALL_SERVICES_AVAILABILITY_IN_ALL_OUTLETS: Joi.boolean().required(),
-
-        PRIV_VIEW_ALL_OUTLETS: Joi.boolean().required(),
-        PRIV_MODIFY_ALL_OUTLETS: Joi.boolean().required(),
-
-        PRIV_VIEW_ALL_WAREHOUSES: Joi.boolean().required(),
-        PRIV_MODIFY_ALL_WAREHOUSES: Joi.boolean().required(),
-
-        PRIV_VIEW_ORGANIZATION_STATISTICS: Joi.boolean().required(),
-        PRIV_MODIFY_ORGANIZATION: Joi.boolean().required(),
-
-        PRIV_VIEW_CUSTOMER: Joi.boolean().required(),
-        PRIV_MODIFY_CUSTOMER: Joi.boolean().required(),
-        PRIV_MANAGE_CUSTOMER_WALLET_BALANCE: Joi.boolean().required(),
-
-        PRIV_VIEW_VENDOR: Joi.boolean().required(),
-        PRIV_MODIFY_VENDOR: Joi.boolean().required(),
-
-        PRIV_VIEW_REPORTS: Joi.boolean().required(),
-
-        PRIV_MODIFY_ORGANIZATION_SETTINGS: Joi.boolean().required()
-      })
+      privileges: getPrivilegesSchemaFromJson()
     });
   }
 
