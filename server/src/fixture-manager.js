57ee7f901725bef1662ec471cec27db7169b4725
Sayem Shafayet <ishafayet@gmail.com>
2019-12-03 18:50:49 +0600

    Demo package to 7  days

--- server/src/fixture-manager.js
+++ server/src/fixture-manager.js
@@ -33,7 +33,7 @@ class FixtureManager {
       {
         name: "package-list",
         path: "./src/fixtures/package-list.json",
-        version: 17
+        version: 18
       },
       {
         name: "module-list",
