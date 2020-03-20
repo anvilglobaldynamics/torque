34240eb8f08aabe6b386e1b5bd752a0edccf9e8f
Sayem Shafayet <ishafayet@gmail.com>
2020-03-20 10:54:19 +0600

    Added AdminSetupAccountingModuleApi

---
+++ server/src/apis/admin-setup-accounting-module.js
@@ -0,0 +1,48 @@
+
+const { Api } = require('./../api-base');
+const Joi = require('joi');
+const { throwOnFalsy, throwOnTruthy, CodedError } = require('./../utils/coded-error');
+const { OrganizationMixin } = require('./mixins/organization-mixin');
+const { AccountingMixin } = require('./mixins/accounting-mixin');
+
+exports.AdminSetupAccountingModuleApi = class extends Api.mixin(OrganizationMixin, AccountingMixin) {
+
+  get autoValidates() { return true; }
+
+  get requiresAuthentication() { return true; }
+
+  get authenticationLevel() { return 'admin'; }
+
+  get isEnabled() { return false; }
+
+  get requestSchema() {
+    return Joi.object().keys({
+      apiKey: Joi.string().length(64).required(),
+      organizationId: Joi.number().max(999999999999999).required()
+    });
+  }
+
+  async handle({ body, username }) {
+    let { organizationId } = body;
+    let organization = await this.database.organization.findById({ id: organizationId });
+    throwOnFalsy(organization, "ORGANIZATION_INVALID", this.verses.organizationCommon.organizationDoesNotExist);
+
+    // Activate MOD_ACCOUNTING if not activated. May not be appropriate after development. Consider removing - start
+    let { activeModuleCodeList } = organization;
+    let organizationHasModule = (activeModuleCodeList.indexOf(moduleCode) > -1);
+    if (!organizationHasModule) {
+      await this.database.moduleActivation.create({ moduleCode: "MOD_ACCOUNTING", organizationId, createdByAdminName: username, paymentReference: "Internal" });
+      activeModuleCodeList.push(moduleCode);
+    }
+    let result = await this.database.organization.setActiveModuleCodeList({ id: organizationId }, { activeModuleCodeList });
+    this.ensureUpdate('organization', result);
+    // - end
+
+    await this.createDefaultAccounts({ organizationId });
+
+    await this._remotelyTerminateSessionOfUsersInOrganization({ organizationId });
+
+    return { status: "success" };
+  }
+
+}
