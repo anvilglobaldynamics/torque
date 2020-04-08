af6253208d69bab26a3a140cbb26bc5e70094cbe
Mujtaba Nowshad <nowshad@anvil.live>
2020-04-08 18:25:36 +0600

    R-DFRESH-01 package made

--- server/src/fixture-manager.js
+++ server/src/fixture-manager.js
@@ -33,7 +33,7 @@ class FixtureManager {
       {
         name: "package-list",
         path: "./src/fixtures/package-list.json",
-        version: 20
+        version: 21
       },
       {
         name: "module-list",
