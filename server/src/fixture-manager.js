ed965656b888164a2876aa30cf67ddc9e46644dc
Sayem Shafayet <ishafayet@gmail.com>
2020-06-21 18:17:46 +0600

    WIP

--- server/src/fixture-manager.js
+++ server/src/fixture-manager.js
@@ -33,7 +33,7 @@ class FixtureManager {
       {
         name: "package-list",
         path: "./src/fixtures/package-list.json",
-        version: 24
+        version: 25
       },
       {
         name: "module-list",
