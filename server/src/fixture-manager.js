10f63590966c54b813fcab922e019d16a7896fac
Sayem Shafayet <ishafayet@gmail.com>
2020-11-14 12:36:35 +0600

    Updated package list

--- server/src/fixture-manager.js
+++ server/src/fixture-manager.js
@@ -33,7 +33,7 @@ class FixtureManager {
       {
         name: "package-list",
         path: "./src/fixtures/package-list.json",
-        version: 26
+        version: 27
       },
       {
         name: "module-list",
